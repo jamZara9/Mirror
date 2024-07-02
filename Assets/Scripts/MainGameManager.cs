f83e54faf11ad6abe98b9f0de33bfcb3feecee3b
Argonaut <85242295+jeonghyeok-Lee@users.noreply.github.com>
2024-07-02 21:06:53 +0900

    add : GameManager 추가

---
+++ Assets/Scripts/MainGameManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainGameManager : MonoBehaviour
+{
+    private static MainGameManager _mainGameManager;              // 싱글톤 인스턴스
+    public static MainGameManager Instance {
+        get{
+            if(_mainGameManager == null){
+                // 씬에서 MainGameManager를 찾음
+                _mainGameManager = FindObjectOfType<MainGameManager>();
+
+                // 씬에 MainGameManager가 없는 경우 새로운 GameObject를 생성하여 추가
+                if (_mainGameManager == null)
+                {
+                    GameObject singleton = new GameObject("MainGameManager");
+                    _mainGameManager = singleton.AddComponent<MainGameManager>();
+                }
+            }
+            return _mainGameManager;
+        }
+    }
+
+    void Awake(){
+        // 싱글톤 인스턴스 설정
+        if(_mainGameManager == null){
+            _mainGameManager = this;                            // 인스턴스 할당
+            DontDestroyOnLoad(gameObject);                  // 씬 전환 시에도 파괴되지 않도록 설정
+        }
+        else{
+            Destroy(gameObject);
+        }
+    }
+
+    
+}
