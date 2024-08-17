33c7f22d9349ffb0e66c514a8befc667ce6358ab
YoimiyaKazusa <kgw4589@gmail.com>
2024-08-17 21:14:56 +0900

    Feat / Horror :: 문 상호작용

--- Assets/Dev_YoimiyaKazusa/HorrorEvent/Scripts/FloorOneFrontDoor.cs
+++ Assets/Dev_YoimiyaKazusa/HorrorEvent/Scripts/FloorOneFrontDoor.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,9 +6,51 @@ using UnityEngine;
 public class FloorOneFrontDoor : MonoBehaviour, IInteractionable
 {
     [SerializeField] private StoryScene myStoryScene;
+
+    private bool _isOpen = false;
     
+    [SerializeField] private float rotateDuration;
+
+    [SerializeField] private Vector3 originRotion;
+    [SerializeField] private Vector3 openRotation;
+
+    private GameController _gameController;
+
+    private void Awake()
+    {
+        _gameController = FindObjectOfType<GameController>();
+    }
+
+    private void Start()
+    {
+        Interaction();
+    }
+
     public void Interaction()
     {
+        _gameController.PlayScene(myStoryScene);
         
+        if (!_isOpen)
+        {
+            _isOpen = true;
+            StartCoroutine(Rotate(originRotion, openRotation));
+        }
+        else
+        {
+            _isOpen = false;
+            StartCoroutine(Rotate(openRotation, originRotion));
+        }
+    }
+
+    IEnumerator Rotate(Vector3 startVector, Vector3 endVector)
+    {
+        float currentTime = 0;
+        
+        while (currentTime < rotateDuration)
+        {
+            currentTime += Time.deltaTime;
+            transform.eulerAngles = Vector3.Lerp(startVector,endVector, currentTime / rotateDuration);
+            yield return null;
+        }
     }
 }
