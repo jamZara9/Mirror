757d250d33f5150f10348b3c446ee273219d03f5
YoimiyaKazusa <kgw4589@gmail.com>
2024-08-17 21:44:09 +0900

    Refactor / Horror :: 문 상호작용 분리하여 단일 클래스 저장

---
+++ Assets/Dev_YoimiyaKazusa/HorrorEvent/Scripts/Door.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Linq;
+using UnityEngine;
+
+[Serializable]
+struct DoorStruct
+{
+    public Vector3 rotation;
+    public float duration;
+}
+
+public class Door : MonoBehaviour, IInteractionable
+{
+    private bool _isOpen = false;
+
+    [SerializeField] private DoorStruct[] doorStruct;
+
+    private void Start()
+    {
+        Interaction();
+    }
+
+    public void Interaction()
+    {
+        if (!_isOpen)
+        {
+            _isOpen = true;
+            StartCoroutine(Rotate());
+        }
+        else
+        {
+            _isOpen = false;
+            StartCoroutine(Rotate());
+        }
+    }
+    
+    IEnumerator Rotate()
+    {
+        foreach (var doorInfo in (_isOpen ? doorStruct : doorStruct.Reverse()))
+        {
+            float currentTime = 0;
+            Vector3 startVector = transform.eulerAngles;
+            
+            while (startVector != doorInfo.rotation && currentTime < doorInfo.duration)
+            {
+                currentTime += Time.deltaTime;
+                transform.eulerAngles = Vector3.Lerp(startVector,doorInfo.rotation, currentTime / doorInfo.duration);
+                yield return null;
+            }
+        }
+    }
+}
