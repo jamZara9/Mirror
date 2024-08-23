a50d8932c5d9b365cf1684db8e4a8cf3917127f4
Argonaut <85242295+jeonghyeok-Lee@users.noreply.github.com>
2024-08-23 23:44:33 +0900

    modify: Item/Weapon Manager 최적화

--- Assets/02.Scripts/Controllers/Items/ItemManager.cs
+++ Assets/02.Scripts/Controllers/Items/ItemManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Newtonsoft.Json;
+using Unity.VisualScripting;
 
 /// <summary>
 /// 아이템을 관리하는 클래스
@@ -10,34 +11,9 @@ using Newtonsoft.Json;
 /// author: Argonaut
 /// </para>
 /// </summary>
-public class ItemManager : MonoBehaviour
+public class ItemManager : BaseItemManager<BaseItem, BaseItemData>
 {
-    [Header("Item Data")]
-    // 아이템 데이터 딕셔너리 [ 아이템 ID, 아이템 데이터 ]
-    public Dictionary<string, BaseItemData> itemDictionary = new Dictionary<string, BaseItemData>();   
-
-    [Header("Item List")]
-    // 필드(씬)에 존재하는 Item 오브젝트들을 저장하는 리스트
-    public List<BaseItem> items = new List<BaseItem>();
-
-    private string itemDataPath = "Json/items"; // 아이템 데이터 경로
-
-    void Awake(){
-
-        // 아이템 정보 로드
-        LoadItemData(itemDataPath);
-        
-        // 모든 BaseItem 타입의 객체를 찾아서 리스트에 추가
-        // BaseItem[] allBaseItems = Resources.FindObjectsOfTypeAll<BaseItem>();
-        BaseItem[] allBaseItems = FindObjectsOfType<BaseItem>(); // 활성화된 오브젝트를 가져옴
-        foreach (BaseItem item in allBaseItems){
-            items.Add(item);                    // 리스트에 추가
-            SetItemData(item);                  // 아이템 데이터 설정
-            SetItemActiveState(item, true);     // 아이템 활성화
-            SetItemIcon(item);                  // 아이템 아이콘 설정
-            Debug.Log($"아이템 추가: {item.itemID}");
-        }
-    }
+    protected override string DataPath => "Json/items"; // 아이템 데이터 경로
     
     // EventManager에 이벤트 핸들러 등록
     private void OnEnable() {
@@ -52,44 +28,38 @@ public class ItemManager : MonoBehaviour
     }
 
     /// <summary>
-    /// 아이템 데이터 로드 함수
+    /// 아이템 데이터 설정 함수
     /// </summary>
-    /// <param name="itemPath">item 데이터가 있는 path</param>
-    public void LoadItemData(string itemPath){
-        string jsonData = FileManager.LoadJsonFile(itemDataPath);       // json 파일 로드
-        if(jsonData != null){
-            // json 데이터 -> 딕셔너리로 변환
-            itemDictionary = JsonConvert.DeserializeObject<Dictionary<string, BaseItemData>>(jsonData);
-            Debug.Log($"아이템 데이터 로드 완료. 아이템 개수: {itemDictionary.Count}");
+    protected override void SetItemData(BaseItem item)
+    {
+        if (dataDictionary.TryGetValue(item.itemID, out var data))
+        {
+            item.itemData = data;
+        }
+        else
+        {
+            Debug.LogWarning($"아이템 데이터가 없습니다: {item.itemID}");
         }
     }
 
     /// <summary>
     /// 아이템 아이콘 설정 함수
     /// </summary>
-    private void SetItemIcon(BaseItem item){
-        BaseItemData data = itemDictionary[item.itemID];
-        try{
-            if(data.iconPath == null || data.iconPath == ""){
-                Debug.LogError("아이템 아이콘 경로가 비어있습니다.");
-                return;
-            }
-
+    protected override void SetItemIcon(BaseItem item)
+    {
+        if (dataDictionary.TryGetValue(item.itemID, out var data) && !string.IsNullOrEmpty(data.iconPath))
+        {
             item.icon = Resources.Load<Sprite>(data.iconPath);
-        }catch(System.Exception e){
-            Debug.LogError($"아이템 아이콘 설정 중 오류 발생: {e}");
+        }
+        else
+        {
+            Debug.LogError($"아이템 데이터가 없거나 아이콘 경로가 비어있습니다: {item.itemID}");
         }
     }
 
-    /// <summary>
-    /// 아이템 데이터 설정 함수
-    /// </summary>
-    /// <param name="item">설정하고자 하는 item</param>
-    private void SetItemData(BaseItem item){
-        BaseItemData data = itemDictionary[item.itemID];
-
-        // 아이템 데이터 설정
-        item.itemData = data;
+    protected override string GetItemID(BaseItem item)
+    {
+        return item.itemID;
     }
 
     /// <summary>
@@ -129,7 +99,7 @@ public class ItemManager : MonoBehaviour
     private void HandleItemPickup(BaseItem detectedItem, PlayerInventory playerInventory){
         // 감지된 아이템이 픽업 가능한지 확인
         if(detectedItem.isPickable){
-            SetItemActiveState(detectedItem, false);           // 아이템 비활성화
+            SetItemActiveState(detectedItem, false);          // 아이템 비활성화
             playerInventory.AddItem(detectedItem);            // 플레이어 인벤토리에 아이템 추가
         }
     }
@@ -157,10 +127,15 @@ public class ItemManager : MonoBehaviour
     /// <param name="to">아이템을 이동시킬 위치</param>
     /// <param name="item">전달하고자 하는 아이템</param>
     public void TransferItem(IItemContainer from, IItemContainer to, BaseItem item){
-        Debug.Log("아이템 이동");
         // @ TODO: 아이템 이동 로직 구현 아직 미완성
-        from.RemoveItem(item);
-        to.AddItem(item);
+
+        if(from != null && to != null){
+            from.RemoveItem(item);
+            to.AddItem(item);
+            Debug.Log("아이템 이동");
+        }else{
+            Debug.LogError("아이템 이동 실패");
+        }
     }
 
 }
--- Assets/02.Scripts/Controllers/Weapon/WeaponManager.cs
+++ Assets/02.Scripts/Controllers/Weapon/WeaponManager.cs
@@ -6,79 +6,60 @@ using Newtonsoft.Json;
 /// <summary>
 /// 무기를 관리하는 클래스
 /// </summary>
-public class WeaponManager : MonoBehaviour
+public class WeaponManager : BaseItemManager<BaseWeapon, BaseWeaponData>
 {
-    [Header("Weapon Data")]
-    // 무기 데이터 딕셔너리 [ 무기 ID, 무기 데이터 ]
-    public Dictionary<string, BaseWeaponData> weaponDictionary = new Dictionary<string, BaseWeaponData>();
-
-    [Header("Weapon List")]
-    // 필드(씬)에 존재하는 Weapon 오브젝트들을 저장하는 리스트
-    public List<BaseWeapon> weapons = new List<BaseWeapon>();
-
-    private string weaponDataPath = "Json/weapons"; // 무기 데이터 경로
-
-    void Awake()
-    {
-
-        // 무기 정보 로드
-        LoadItemData(weaponDataPath);
-
-        // 모든 BaseWeapon 타입의 객체를 찾아서 리스트에 추가
-        // BaseWeapon[] allBaseWeapons = Resources.FindObjectsOfTypeAll<BaseWeapon>();
-        BaseWeapon[] allBaseWeapons = FindObjectsOfType<BaseWeapon>(); // 활성화된 오브젝트를 가져옴
-        foreach (BaseWeapon weapon in allBaseWeapons)
-        {
-            weapons.Add(weapon);                    // 리스트에 추가
-            SetItemData(weapon);                  // 무기 데이터 설정
-            // SetWeaponActiveState(weapon, true);     // 무기 활성화
-            SetItemIcon(weapon);                  // 무기 아이콘 설정
-            Debug.Log($"무기 추가: {weapon.weaponID}");
-        }
-    }
-
-    /// <summary>
-    /// 무기 데이터 로드 함수
-    /// </summary>
-    /// <param name="itemPath">item 데이터가 있는 path</param>
-    public void LoadItemData(string itemPath)
-    {
-        string jsonData = FileManager.LoadJsonFile(weaponDataPath);       // json 파일 로드
-        if (jsonData != null)
-        {
-            // json 데이터 -> 딕셔너리로 변환
-            weaponDictionary = JsonConvert.DeserializeObject<Dictionary<string, BaseWeaponData>>(jsonData);
-            Debug.Log($"아이템 데이터 로드 완료. 아이템 개수: {weaponDictionary.Count}");
-        }
-    }
+     protected override string DataPath => "Json/weapons"; // 무기 데이터 경로
 
     /// <summary>
     /// 무기 데이터 설정 함수
     /// </summary>
     /// <param name="item">설정하고자 하는 item</param>
-    private void SetItemData(BaseWeapon item)
+    protected override void SetItemData(BaseWeapon item)
     {
-        BaseWeaponData data = weaponDictionary[item.weaponID];
-
-        // 아이템 데이터 설정
-        item.weaponData = data;
+        if (dataDictionary.TryGetValue(item.weaponID, out var data))
+        {
+            item.weaponData = data;
+        }
+        else
+        {
+            Debug.LogWarning($"무기 데이터가 없습니다: {item.weaponID}");
+        }
     }
 
     /// <summary>
     /// 아이템 아이콘 설정 함수
     /// </summary>
-    private void SetItemIcon(BaseWeapon item){
-        BaseWeaponData data = weaponDictionary[item.weaponID];
-        try{
-            if(data.iconPath == null || data.iconPath == ""){
-                Debug.LogError("아이템 아이콘 경로가 비어있습니다.");
-                return;
-            }
-
+    protected override void SetItemIcon(BaseWeapon item)
+    {
+        // if (dataDictionary.TryGetValue(item.weaponID, out var data))
+        // {
+        //     if (!string.IsNullOrEmpty(data.iconPath))
+        //     {
+        //         item.icon = Resources.Load<Sprite>(data.iconPath);
+        //     }
+        //     else
+        //     {
+        //         Debug.LogError("무기 아이콘 경로가 비어있습니다.");
+        //     }
+        // }
+        // else
+        // {
+        //     Debug.LogWarning($"무기 데이터가 없습니다: {item.weaponID}");
+        // }
+        if (dataDictionary.TryGetValue(item.weaponID, out var data) && !string.IsNullOrEmpty(data.iconPath))
+        {
             item.icon = Resources.Load<Sprite>(data.iconPath);
-        }catch(System.Exception e){
-            Debug.LogError($"아이템 아이콘 설정 중 오류 발생: {e}");
         }
+        else
+        {
+            Debug.LogError($"아이템 데이터가 없거나 아이콘 경로가 비어있습니다: {item.weaponID}");
+        }
+    }
+
+
+    protected override string GetItemID(BaseWeapon item)
+    {
+        return item.weaponID;
     }
 
 
