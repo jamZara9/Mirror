3abee865b85da9540e324f3085f9b6cdf483a7c1
Argonaut <85242295+jeonghyeok-Lee@users.noreply.github.com>
2024-09-13 15:36:36 +0900

    fix: 애니메이션 사운드가 정상적으로 출력되지 않던 문제 해결

--- Assets/02.Scripts/Player/PlayerStateController.cs
+++ Assets/02.Scripts/Player/PlayerStateController.cs
@@ -46,9 +46,6 @@ public class PlayerStateController : MonoBehaviour
 
     [Header("Audio Settings")]
     [SerializeField] private AudioSource audioSource;           // 오디오 소스
-    [SerializeField] private AudioClip landingAudioClip;        // 착지 사운드
-    [SerializeField] private AudioClip[] footstepAudioClips;    // 발소리 사운드
-    [Range(0, 1)] public float footstepAudioVolume = 0.5f;      // 발소리 사운드 볼륨
     [SerializeField] private AudioClip attackAudioClip;         // 공격 사운드
 
 
@@ -641,36 +638,6 @@ public class PlayerStateController : MonoBehaviour
             _3stAnimator.SetBool(_animIDAttack, false);
         }
     }
-
-    #region Animation Events
-    /// <summary>
-    /// 발소리 이벤트
-    /// </summary>
-    public void OnFootstep(AnimationEvent animationEvent)
-    {
-        Debug.Log("Footstep event triggered");
-        if (animationEvent.animatorClipInfo.weight > 0.5f)
-        {
-            if (footstepAudioClips.Length > 0)
-            {
-                var index = UnityEngine.Random.Range(0, footstepAudioClips.Length);
-                AudioSource.PlayClipAtPoint(footstepAudioClips[index], transform.TransformPoint(_characterController.center), footstepAudioVolume);
-            }
-        }
-    }
-
-    /// <summary>
-    /// 착지 이벤트
-    /// </summary>
-    /// <param name="animationEvent"></param>
-    public void OnLand(AnimationEvent animationEvent)
-    {
-        if (animationEvent.animatorClipInfo.weight > 0.5f)
-        {
-            AudioSource.PlayClipAtPoint(landingAudioClip, transform.TransformPoint(_characterController.center), footstepAudioVolume);
-        }
-    }
-    #endregion
 }
 
 
