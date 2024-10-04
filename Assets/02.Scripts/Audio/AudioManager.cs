11fa3067fdb30aa3f765368e822a95d837e34742
Argonaut <85242295+jeonghyeok-Lee@users.noreply.github.com>
2024-10-04 16:48:18 +0900

    refactor: AudioManager 리팩토링

--- Assets/02.Scripts/Audio/AudioManager.cs
+++ Assets/02.Scripts/Audio/AudioManager.cs
@@ -1,24 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Rendering;
 using UnityEngine;
 
-public static class AudioConstants
+public class AudioManager : IManager
 {
-    public const int BGM_STARTSCENE = 0;
-    public const int BGM_PLAYGROUNDA = 1;
-}
-
-public class AudioManager : Singleton<AudioManager>, IManager
-{
-    [SerializeField] private AudioSource bgmSource;         // 배경음악 소스
-    [SerializeField] private AudioClip[] bgmClips;          // 배경음악 클립들
-
+    private AudioSource bgmSource;         // 배경음악 소스
     public void Initialize(string sceneName)
     {
-        if(bgmSource == null)
+       
+    }
+
+    /// <summary>
+    /// 배경음악 클립들을 설정
+    /// </summary>
+    /// <param name="source">배경음이 출력될 AudioSource</param>
+    public void SetBGMSource(AudioSource source)
+    {
+        if(source == null)
         {
-            bgmSource = gameObject.AddComponent<AudioSource>();
+            Debug.LogError("AudioSource가 설정되지 않았습니다.");
+            return;
         }
+        
+        bgmSource = source;
     }
 
     /// <summary>
@@ -29,19 +34,26 @@ public class AudioManager : Singleton<AudioManager>, IManager
     /// <param name="volume">사운드 크기</param>
     public void PlaySoundEffect(AudioClip clip, Vector3 position, float volume)
     {
-        // 사운드 클립이 없으면 리턴
-        if(clip == null) return;
+        if(clip == null)
+        {
+            Debug.LogError("재생할 AudioClip이 없습니다.");
+            return;
+        }
+
+        if(position == null)
+        {
+            Debug.LogError("재생 위치가 없습니다.");
+            return;
+        }
+
+        if(volume < 0.0f || volume > 1.0f)
+        {
+            Debug.LogError("볼륨이 올바르지 않습니다.");
+            return;
+        }
+        
 
         AudioSource.PlayClipAtPoint(clip, position, volume);
-    }
-
-    public void PlayBackgroundMusic(int index)
-    {
-        try{
-            PlayBackgroundMusic(bgmClips[index], 1.0f);
-        }catch(System.IndexOutOfRangeException e){
-            Debug.LogError("인덱스 범위를 넘어감: " + e.Message);
-        }
     }
 
     public void PlayBackgroundMusic(AudioClip clip)
@@ -56,6 +68,17 @@ public class AudioManager : Singleton<AudioManager>, IManager
     /// <param name="volume">사운드 크기</param>
     public void PlayBackgroundMusic(AudioClip clip, float volume)
     {
+        if(bgmSource == null)
+        {
+            Debug.LogError("BGM AudioSource가 설정되지 않았습니다.");
+            return;
+        }
+
+        if(clip == null)
+        {
+            Debug.LogError("재생할 AudioClip이 없습니다.");
+            return;
+        }
         bgmSource.clip = clip;
         bgmSource.volume = volume;
         bgmSource.loop = true;
